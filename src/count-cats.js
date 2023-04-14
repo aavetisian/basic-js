dc8b77419b338d1007f77c3f6bdcaf26877f85ec
Ani <aavetisian@noorlogic.com>
2023-04-14 21:19:38 +0400

    feat: 2 problems solved

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function countCats(matrix) {
+//   const count = matrix.reduce((a, v) => a + v.includes('^^'), 0)
+//   return count
+// }
+function countCats(backyard) {
+  // Flatten the array into a one-dimensional array
+  const flattened = backyard.flat();
+
+  // Count the number of cats (i.e. the number of occurrences of "^^")
+  const count = flattened.filter(item => item === '^^').length;
+
+  return count;
 }
 
 module.exports = {
   countCats
 };
+// console.log(countCats([
+//   ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+//   [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+//   [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+//   [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+//   [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+//   [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+//   [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+//   [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"]
+// ]));
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function createDreamTeam(members) {
+//   if (typeof members.map(v => v )!=='string'){
+//     return false
+//   }
+//   const team =members.map(v=>v.slice(0, 1)).sort().join('').toUpperCase()
+//   return team
+// }
+function createDreamTeam(members) {
+  // Check if the input is an array
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  // Filter out non-string values and extract the first letter of each name
+  const initials = members.filter(member => typeof member === 'string')
+    .map(member => member.trim()[0].toUpperCase());
+
+  // Sort the initials alphabetically and concatenate them into a string
+  const name = initials.sort().join('');
+
+  return name;
 }
 
 module.exports = {
